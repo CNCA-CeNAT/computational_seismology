a8a0515e2d4c02a0a2f61396c4b0db648cbf4178
Guillermo Cornejo <gcornejo@cenat.ac.cr>
2017-07-21 09:43:54 -0600

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# framework-sismologia
+# Framework para la simulación en paralelo de fenómenos sísmocos y volcánicos
+
+## Pre-simulación
+
+*order\_xyz.py* Toma un archivo xyz, tal como lo exporta QGIS y lo transforma al formato adecuado para simular con specfem3D.
+
+*cmp_synth* Toma una colección de sismogramas, empacados en un .mseed y los compara con los sismogramas sintéticos contenidos en un directorio. Los nombres de los sismogramas sintéticos y observados deben coincidir. 
---
+++ cmp_synth.py
@@ -0,0 +1,83 @@
+#!/opt/intel/intelpython3/bin/python
+
+import matplotlib.pyplot as plt
+import numpy as np
+import obspy 
+import sys
+import os
+import re
+
+Trillium_seimometer_response = {
+    'poles':[-272+218j, -272-218j, 56.5+0j, -0.1111-0.1111j, -0.0000+0.1111j],
+    'zeros':[0j, 0j, 51.5+0j],
+    'gain' :133310,
+    'sensitivity':1500
+}
+
+def match_streams(stream, files):
+    file_name = stream.stats['network'] + '.' + stream.stats['station'] + '.[A-Z]+' + stream.stats['channel'][-1] + '.sema'
+    for f in files:
+        if re.search(file_name, f):
+            return f
+    return None
+
+def print_usage_exit():
+    print('Usage: %s observed_seismograms.mseed path_to_synths synth_step' % sys.argv[0])
+    sys.exit(1)
+
+
+def main():
+    try:
+        recorded = obspy.read(sys.argv[1])
+        directory = sys.argv[2]
+        files = [f for f in os.listdir(directory) if os.path.isfile(os.path.join(directory, f))]
+        synth_step = float(sys.argv[3])
+    except OSError:
+        print("Error openning file or directory")
+        print_usage_exit()
+    except IndexError:
+        print('Wrong number of arguments')
+        print_usage_exit()
+    except ValueError:
+        print('Check synth_step format')
+        print_usage_exit()
+
+    # Match all observed seismograms with a synthetic one in "directory"
+    synths   = []
+    observed = []
+    for stream in recorded:
+        synth_file = match_streams(stream, files)
+        if not synth_file:
+            print('Stream %s has no math in directory %s' % (str(stream), directory))
+        else:
+            observed.append(stream)
+        synth_data = np.loadtxt(os.path.join(directory, synth_file))
+        synth = obspy.Trace(synth_data[:,1])
+        synth.stats['delta'] = synth_step 
+        synths.append(synth)
+    del recorded
+
+    # Resample to the smallest sample rate i.e. biggest step (delta)
+    sample_rate = max(observed[0].stats['delta'], synths[0].stats['delta'])
+    if observed[0].stats['delta'] > synths[0].stats['delta']:
+        new_delta = observed[0].stats['delta']
+        resample = synths
+    else:
+        new_delta = synths[0].stats['delta']
+        resample = observed
+
+    for trace in resample:
+        trace.resample(1/new_delta)
+
+    # Remove the trend and taper observed seismograms
+    # Remove instrument response and sensivity
+    for stream in observed:
+        stream.detrend(type='linear')
+        stream.simulate(paz_remove=Trillium_seimometer_response, remove_sensitivity=True, water_level=0, taper_fraction=0.5)
+    
+    # Low-pass filter
+    for stream in observed:
+        
+
+if __name__ == '__main__':
+    main()
