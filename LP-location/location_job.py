c4879f4d164a262481c06954f9af9330a56af8ad
Guillermo Cornejo Suárez <gcornejo@cenat.ac.cr>
2018-04-10 15:33:10 -0600

    new amplitudes experiment

--- LP-location/location_job.py
+++ LP-location/location_job.py
@@ -1,8 +1,8 @@
 #!/opt/intel/intelpython35/bin/python3
 
 #PBS -N tremor_location
-#PBS -q phi-n2h72
-#PBS -l nodes=2:ppn=64
+#PBS -q phi-n3h24
+#PBS -l nodes=3:ppn=64
 #PBS -l walltime=23:00:00 
 
 import modules
@@ -10,9 +10,9 @@ import sys
 
 modules.load('intelpython/3.5')
 
-exe = '/home/gmocornejos/computational_seismology/LP-location/body_waves_topology/location_topo_mpi.py'
+exe = '/home/gmocornejos/computational_seismology/LP-location/body_waves_topography/location_topo_mpi.py'
 
-conf = '/home/gmocornejos/computational_seismology/LP-location/body_waves_topology/location.conf'
+conf = '/home/gmocornejos/computational_seismology/LP-location/body_waves_topography/location.conf'
 
 try:
     configuration = sys.argv[1]
